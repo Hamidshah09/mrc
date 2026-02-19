7a2d4977a15a9d726fe0b8c4b5974e5e1295c23c
Hamid Shah <hamidshah09@gmail.com>
2026-02-19 23:25:39 +0500

    minor changes

--- app/Http/Controllers/PostalServiceExportController.php
+++ app/Http/Controllers/PostalServiceExportController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 use App\Models\PostalService;
 use App\Models\PostalStatuses;
+use App\Models\City;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Barryvdh\DomPDF\Facade\Pdf;

f6ec8d7bd0f72495687080952fd5a018ef31ddbc
Hamid Shah <hamidshah09@gmail.com>
2026-02-24 22:53:23 +0500

    envelop buttion restored

--- resources/views/postalservice/index.blade.php
+++ resources/views/postalservice/index.blade.php
@@ -16,6 +16,15 @@
                 <button type="submit" class="mb-2 px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">Export Report</button>
             </form>
             <a href="{{route('postalservice.send_to_po')}}" class="ml-2 mb-2 px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">Send to PO</a>
+            <a href="#"
+            onclick="openEnvelopeLabelModal(); return false;"
+            class="ml-2 mb-2 px-4 py-2 bg-indigo-600 border border-transparent
+                    rounded-md font-semibold text-xs text-white uppercase tracking-widest
+                    hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900
+                    focus:outline-none focus:ring-2 focus:ring-indigo-500
+                    focus:ring-offset-2 transition ease-in-out duration-150">
+                Envelope Labels
+            </a>
         </div>
         
         <!-- Envelope Label Modal -->

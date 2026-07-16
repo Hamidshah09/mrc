f41332efeb960f0d2680a135ddf1e39f88f98c33
Hamid Shah <hamidshah09@gmail.com>
2026-07-16 15:01:04 +0500

    challan updated

--- app/Http/Controllers/CashRecordController.php
+++ app/Http/Controllers/CashRecordController.php
@@ -402,6 +402,10 @@ class CashRecordController extends Controller
             $query->where('service_type', $request->input('service_type'));
         }
 
+        if ($request->filled('payment_type')) {
+            $query->where('payment_type', $request->input('payment_type'));
+        }
+        
         if ($request->filled('q')) {
             $q = $request->input('q');
             $query->where(function ($qb) use ($q) {
